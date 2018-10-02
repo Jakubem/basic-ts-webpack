b29b5b78716e0cbd9db755b0c502248da29d928b
unknown <jakubmx@gmail.com>
2018-10-02 16:26:14 +0200

    add dev server, friendly errors, html webpack plugin

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-/* TypeScript hello world */
+/* hello world */
 
 function greeter(person:any) {
     return "Hello, " + person;
 }
 let user = "Jane User";
+
 document.body.innerHTML = greeter(user);
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,19 @@
 const path = require('path');
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const minify = {
+  removeComments: true,
+  collapseWhitespace: true,
+  removeRedundantAttributes: true,
+  useShortDoctype: true,
+  removeEmptyAttributes: true,
+  removeStyleLinkTypeAttributes: true,
+  keepClosingSlash: true,
+  minifyJS: true,
+  minifyCSS: true,
+  minifyURLs: true,
+}
 module.exports = {
   entry: './src/index.ts',
   module: {
@@ -14,9 +28,28 @@ module.exports = {
   resolve: {
     extensions: [ '.tsx', '.ts', '.js' ]
   },
+  stats: 'errors-only',
+  devServer: {
+    stats: 'errors-only',
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 5500,
+    historyApiFallback: true,
+  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  plugins: [
+    new FriendlyErrorsWebpackPlugin({
+      clearConsole: true,
+    }),
+    new HtmlWebpackPlugin({
+      hash: false,
+      minify: minify,
+      template: './src/index.html',
+      filename: './index.html'
+    }),
+  ],
 };
  
